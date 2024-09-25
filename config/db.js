584ba513e8dcd12856ba4be6773c7db3ee2adc69
Aman <developer@codelinear.com>
2024-09-25 17:25:35 +0530

    mongodb url added

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
+// Replace `myDatabaseName` with the name of the database you want to use
+const uri =
+  "mongodb+srv://amankapil004:tM6mIC3CPrrlEBrR@cluster0.tu4xd.mongodb.net/banner?retryWrites=true&w=majority&appName=Cluster0";
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://amankapil60:amankapil60 @cluster0.mtbsu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection failed:", err.message);
